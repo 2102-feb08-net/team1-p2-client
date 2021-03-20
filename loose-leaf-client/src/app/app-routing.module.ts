9132d6b82ccb047c6d241b04414f8b1c8023b3e9
John Werner <jwerner547@gmail.com>
2021-03-20 15:36:29 -0600

    adding routes for the other components

--- loose-leaf-client/src/app/app-routing.module.ts
+++ loose-leaf-client/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookSearchComponent } from './components/book-search/book-search.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UserLoansComponent } from './components/user-loans/user-loans.component';
+import { UserSearchComponent } from './components/user-search/user-search.component';
 
 const routes: Routes = [
   {
@@ -16,6 +19,18 @@ const routes: Routes = [
   {
     path: "books",
     component: BookSearchComponent
+  },
+  {
+    path: "lenders",
+    component: UserSearchComponent
+  },
+  {
+    path: "lenders/:username",
+    component: UserDetailsComponent
+  },
+  {
+    path: "loans",
+    component: UserLoansComponent
   }
 ];
 
